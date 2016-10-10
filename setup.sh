58d1ec9e9338adf7bb98c54248bbb276419cf93c
Colin Bullock <colin@getset.com>
2016-10-10 16:01:58 -0500

    Update brew script and add setup wrapper

---
+++ setup.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+ touch $HOME/dotfiles/secret.sh
+
+ for FILE in .bash_profile .bashrc .gitconfig .gitignore .tmux.conf .vim .vimrc bash_completion.d; do
+   ln -sf $HOME/dotfiles/$FILE $HOME/$FILE
+ done
+
+ . ~/dotfiles/.osx
+ . ~/dotfiles/.brew
+
